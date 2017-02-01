438de79bba8f3214c27b9e0abc1ff1f61c8a3c13
Tim Spinks <tim@ninthlink.com>
2017-02-01 14:19:56 -0800

    css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,30 +12,15 @@
     </head>
     
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
             
-            @include('partials.nav')
+        @include('partials.nav')
             
-            <div class="content">
-                <div class="container-fluid">
-                
-                    @yield('content')
+        <div class="container-fluid">
+            
+            @yield('content')
 
-                </div>
-            </div>
-        
         </div>
-
+        
         @include('partials.footer')
 
     </body>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,7 @@
 <div class="row">
   <div class="col-sm-12 col-md-6 col-lg-4"></div>
   <div class="col-sm-12 col-md-6 col-lg-8">
-    <h1>Back-end Developer.</h1>
+    <h1>Web Developer.</h1>
     <a href="#" class="btn btn-default btn-lg ripple">Contact</a>
   </div>
 </div>
