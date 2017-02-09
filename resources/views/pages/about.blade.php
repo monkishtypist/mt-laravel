0f018e0d37c024e1d5ffb934046359fac24d7b83
Tim Spinks <tim@ninthlink.com>
2017-02-09 10:59:59 -0800

    css and layout updates

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,11 +7,28 @@
 @section('content')
 
 <div class="row">
-    <div class="col-md-12">
-      <!-- start slipsum code -->
-      Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
-      <!-- end slipsum code -->
-    </div>
+  <div class="col-sm-12 col-md-6 col-lg-4"></div>
+  <div class="col-sm-12 col-md-6 col-lg-8">
+    <h1>Monkishtypist</h1>
+    <a href="mailto:tim@monkishtypist.com">email</a>
+    <dl class="dl-horizontal">
+      <dt>About</dt>
+      <dd>Born near the bay</dd>
+      <dd>Moved to San Diego in the 80's</dd>
+      <dd>Trained as an Economist</dd>
+      <dd>Prefers puzzles and problem solving</dd>
+      <dd>And writing code to solve those puzzles</dd>
+      <dd>But still loves math</dd>
+    </dl>
+    <dl class="dl-horizontal">
+      <dt>Mad Skillz</dt>
+      <dd>Back-end development</dd>
+      <dd>PHP frameworks, APIs</dd>
+      <dd>Custom plugin development</dd>
+      <dd>E-commerce, WooCommerce, WordPress pro</dd>
+      <dd>JIRA admin expert</dd>
+    </dl>
+  </div>
 </div>
 
 @stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,8 +9,9 @@
 <div class="row">
   <div class="col-sm-12 col-md-6 col-lg-4"></div>
   <div class="col-sm-12 col-md-6 col-lg-8">
-    <h1>Web Developer.</h1>
-    <a href="#" class="btn btn-default btn-lg ripple">Contact</a>
+    <h1>APIs<br> Interwebs<br> Frameworks<br> Developement</h1>
+    <!-- <a href="#" class="btn btn-default btn-lg ripple">Contact</a> -->
+    <a href="https://github.com/monkishtypist" target="_blank"><img src="/images/GitHub-Mark-Light-64px.png" alt="Monkishtypist on GitHub" class="src"></a>
   </div>
 </div>
 
